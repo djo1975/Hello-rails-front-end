1cac029568ec722259bbdfed98590e304205aa65
djo1975 <mladanilic1975@gmail.com>
2023-06-13 20:47:35 +0200

    Initial commit

---
+++ src/Greeting.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Greeting() {
+  const [greeting, setGreeting] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/greetings/random')
+      .then(response => {
+        setGreeting(response.data.greeting);
+      })
+      .catch(error => {
+        console.error('Error fetching greeting:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Random Greeting</h1>
+      <p>{greeting}</p>
+    </div>
+  );
+}
+
+export default Greeting;
